ff68e10e91c6eeffeaf748779c1256ff4f70bcb9
Patrice FERLET <metal3d@gmail.com>
2011-09-24 14:11:22 +0200

    Remove class, this is better to use it as a simple module

--- quvi.pyx
+++ quvi.pyx
@@ -14,49 +14,51 @@ __license__="LGPLv2.1+."
 
 cimport cquvi
 
-cdef class Quvi:
-    """Main class that parse media url
-    """
-    #those handles ctypes from quvi.h
-    cdef cquvi.quvi_t _c_quvi
-    cdef cquvi.quvi_media_t _c_m
+#cdef class Quvi:
+"""Main class that parse media url
+"""
+#those handles ctypes from quvi.h
+cdef cquvi.quvi_t _c_quvi
+cdef cquvi.quvi_media_t _c_m
 
+def __cinit__ ():
+    """Initialize quvi handle"""
+    cquvi.quvi_init(&_c_quvi)
 
-    def __cinit__ (self):
-        """Initialize quvi handles"""
-        cquvi.quvi_init(&self._c_quvi)
+def parse(url):
+    """Parse given url parameters"""
+    cdef char* _url = url
+    rc = cquvi.quvi_parse(_c_quvi, _url, &_c_m);
+    if rc != cquvi.QUVI_OK:
+        raise "Exception occured, next media error"
 
-    def parse(self, url):
-        """Parse given url parameters"""
-        cdef char* _url = url
-        rc = cquvi.quvi_parse(self._c_quvi, _url, &self._c_m);
-        if rc != cquvi.QUVI_OK:
-            raise "Exception occured, next media error"
+def getproperties():
+    """Return a dict with title and media url"""
+    cdef char* media_title
+    cdef char* media_url
+    cquvi.quvi_getprop(_c_m, cquvi.QUVIPROP_PAGETITLE, &media_title)
+    cquvi.quvi_getprop(_c_m, cquvi.QUVIPROP_MEDIAURL, &media_url)
+    t= media_title
+    u= media_url
+    return {
+        'url': u,
+        'title': t
+        }
+    
+def next():
+    """Jumps to next media"""
+    rc = cquvi.quvi_next_media_url(&_c_m)
+    if rc != cquvi.QUVI_OK:
+        return False
 
-    def getproperties(self):
-        """Return a dict with title and media url"""
-        cdef char* media_title
-        cdef char* media_url
-        cquvi.quvi_getprop(self._c_m, cquvi.QUVIPROP_PAGETITLE, &media_title)
-        cquvi.quvi_getprop(self._c_m, cquvi.QUVIPROP_MEDIAURL, &media_url)
-        t= media_title
-        u= media_url
-        return {
-            'url': u,
-            'title': t
-            }
-        
-    def next(self):
-        """Jumps to next media"""
-        rc = cquvi.quvi_next_media_url(&self._c_m)
-        if rc != cquvi.QUVI_OK:
-            return False
+    return True
+    
 
-        return True
-        
-    def __del__(self):
-        """Cleanup media handles"""
-        cquvi.quvi_parse_close(&self._c_m);
-        cquvi.quvi_close(&self._c_quvi)
-        cquvi.quvi_free(&self._c_quvi)
+def __del__():
+    """Cleanup media handles"""
+    cquvi.quvi_parse_close(&_c_m);
+    cquvi.quvi_close(&_c_quvi)
+    cquvi.quvi_free(&_c_quvi)
 
+#call initialisation
+__cinit__()
