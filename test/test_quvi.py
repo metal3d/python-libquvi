8ec86935d5ae865f49e5177e34ac4198fc955705
Tom Vincent <tlvince@gmail.com>
2011-11-21 20:18:00 +0000

    Add nose test cases

---
+++ test/test_quvi.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+# Copyright 2011 Tom Vincent <http://tlvince.com/contact/>
+
+import quvi
+
+"""Nose test cases for python-quvi."""
+
+def testInvalidURL():
+    """Test a non-supported URL."""
+    q = quvi.Quvi()
+    try:
+        q.parse("http://google.com")
+    except quvi.QuviError:
+        assert True
+
+def testValidURL():
+    """Test a known-supported URL."""
+    q = quvi.Quvi()
+    q.parse("http://www.youtube.com/watch?v=0gzA6Xzbh1k")
+
+    expected = "youtube"
+    actual = q.getproperties()["hostid"]
+
+    assert actual == expected
