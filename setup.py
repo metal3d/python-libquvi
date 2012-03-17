f0a5de266f22edcb58341d4770c09db0fa919261
Patrice FERLET <metal3d@gmail.com>
2012-03-17 16:58:12 +0100

    Set to Beta - it seemes to be more stable

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ setup(
   url="https://github.com/metal3d/python-libquvi",
   description="libquvi wrapper module",
   license="LGPLv2.1+",
-  version="0.0.1-a3",
+  version="0.0.1-b1",
   cmdclass = {'build_ext': build_ext},
   ext_modules = ext_modules,
   long_description = "".join(open('README.rst').readlines()),
 
   classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
         'Operating System :: POSIX :: Linux',
